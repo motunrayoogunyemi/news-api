77db8f9a71ea22086f9e2d7ab5aab68fc3280f41
Motunrayo Ogunyemi <ogunyemiimotunrayo@gmail.com>
2022-01-14 17:13:56 +0100

    few changes

--- app/app/settings.py
+++ app/app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='foo')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG',default=0))
 
-ALLOWED_HOSTS = ['newss-api.herokuapp.com']
+ALLOWED_HOSTS = ['newss-api.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
