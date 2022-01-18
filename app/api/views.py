edf712b281fc45e65881be3b0159e75e2244a3eb
Motunrayo Ogunyemi <ogunyemiimotunrayo@gmail.com>
2022-01-18 16:00:57 +0100

    Implement multithreading

--- app/api/views.py
+++ app/api/views.py
@@ -1,4 +1,4 @@
-import schedule, time
+import schedule, time, threading
 from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
@@ -117,13 +117,36 @@ class Upvote(APIView):
         myposts.post_upvotes = 0
         return Response('updated')
         
+    def run_continuously(interval=1):
+        """Continuously run, while executing pending jobs at each
+        elapsed time interval.
+        @return cease_continuous_run: threading. Event which can
+        be set to cease continuous run. Please note that it is
+        *intended behavior that run_continuously() does not run
+        missed jobs*. For example, if you've registered a job that
+        should run every minute and you set a continuous run
+        interval of one hour then your job won't be run 60 times
+        at each interval but only once.
+        """
+        cease_continuous_run = threading.Event()
 
-    schedule.every(1).day.at("00:00").do(recurringjob)
+        class ScheduleThread(threading.Thread):
+            @classmethod
+            def run(cls):
+                while not cease_continuous_run.is_set():
+                    schedule.run_pending()
+                    time.sleep(interval)
 
-    
-    while False:
-        schedule.run_pending()
-        time.sleep(1)
+        continuous_thread = ScheduleThread()
+        continuous_thread.start()
+        return cease_continuous_run
+
+
+    schedule.every().day.at("00:00").do(recurringjob)
+
+    #Multithreading prolly
+    # Start the background thread
+    stop_run_continuously = run_continuously()
 
         
 
