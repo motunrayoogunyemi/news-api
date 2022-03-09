b07014c49fca7cea6b86661ace0be310519e94e7
Motunrayo Ogunyemi <ogunyemiimotunrayo@gmail.com>
2022-03-09 01:41:05 +0100

    Use dj database module

--- app/app/settings.py
+++ app/app/settings.py
@@ -78,13 +78,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
